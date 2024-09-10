7d2d7765d3370fc3b6ba1894e1c4f2a372acae7d
patrick mvula <mvlapatrick@gmail.com>
2024-09-10 13:42:18 +0200

    feat: config

---
+++ worker/src/config.ts
@@ -0,0 +1,18 @@
+export const CORS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, HEAD, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Max-Age": "86400",
+};
+
+export const workerResponse = (
+  body: object | string,
+  type: "application/json" | "text/html;charset=UTF-8"
+) => {
+  return new Response(typeof body === "object" ? JSON.stringify(body) : body, {
+    headers: {
+      ...CORS,
+      "Content-Type": type,
+    },
+  });
+};
